9a5df4a879dd93af695f544b21d8de316d6ba408
FeiYee1129 <sharonfish2010@hotmail.com>
2021-05-03 19:51:22 +0800

    change on templatedir

--- Malocclusion_FYP/settings.py
+++ Malocclusion_FYP/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Malocclusion_FYP.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['../classify_system/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
